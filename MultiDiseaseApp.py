6def90594c7de846ad3496e4eb91151437416e53
Naman Shukla <130485795+nmnshukla@users.noreply.github.com>
2025-05-07 21:40:04 +0530

    Add files via upload

---
+++ MultiDiseaseApp.py
@@ -0,0 +1,141 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved model
+HeartDisease_model = pickle.load(open('HeartDisease_model.sav', 'rb'))
+
+Parkinson_model = pickle.load(open('Parkinson_model.sav', 'rb'))
+
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction System',
+                           ['Heart Disease Prediction',
+                            'Parkinsons Prediction'],
+                           icons = ['activity','person'],
+                          
+                           default_index= 0)
+
+if selected == 'Heart Disease Prediction':
+    st.title('Heart Disease Prediction using ML')
+
+    age = st.text_input('Age')
+    sex = st.text_input('Sex')
+    cp = st.text_input('Chest Pain types')
+    trestbps = st.text_input('Resting Blood Pressure')
+    chol = st.text_input('Serum Cholestoral in mg/dl')
+    fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    restecg = st.text_input('Resting Electrocardiographic results')
+    thalach = st.text_input('Maximum Heart Rate achieved')
+    exang = st.text_input('Exercise Induced Angina')
+    oldpeak = st.text_input('ST depression induced by exercise')
+    slope = st.text_input('Slope of the peak exercise ST segment')
+    ca = st.text_input('Major vessels colored by flourosopy')
+    thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+
+    heart_diagnosis = ''
+
+    if st.button('Heart Disease Test Result'):
+
+        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+
+        user_input = [float(x) for x in user_input]
+
+        heart_prediction = HeartDisease_model.predict([user_input])
+
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
+        else:
+            heart_diagnosis = 'The person does not have any heart disease'
+
+    st.success(heart_diagnosis)
+
+if selected == "Parkinsons Prediction":
+    st.title("Parkinson's Disease Prediction using ML")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+
+    with col5:
+        NHR = st.text_input('NHR')
+
+    with col1:
+        HNR = st.text_input('HNR')
+
+    with col2:
+        RPDE = st.text_input('RPDE')
+
+    with col3:
+        DFA = st.text_input('DFA')
+
+    with col4:
+        spread1 = st.text_input('spread1')
+
+    with col5:
+        spread2 = st.text_input('spread2')
+
+    with col1:
+        D2 = st.text_input('D2')
+
+    with col2:
+        PPE = st.text_input('PPE')
+
+    parkinsons_diagnosis = ''
+  
+    if st.button("Parkinson's Test Result"):
+
+        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
+                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
+                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+
+        user_input = [float(x) for x in user_input]
+
+        parkinsons_prediction = Parkinson_model.predict([user_input])
+
+        if parkinsons_prediction[0] == 1:
+            parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
+    st.success(parkinsons_diagnosis)
